43ce34d0b6b4aef1e4c9a80e3d61299949ee2706
Javier Castro <jc@javiercastro.xyz>
2021-03-12 18:22:55 -0500

    Defined systems and forces in `physics.jl`.

--- physics.jl
+++ physics.jl
@@ -1,13 +1,58 @@
 import Base: @kwdef
 
-@kwdef struct Particle
+
+# Define Force, Particle, and System
+
+abstract type Force end
+
+@kwdef mutable struct System
+	state::Dict = Dict()
+end
+
+function create_particle(S::System;
+		mass = 1,
+		charge = 0,
+		radius = 0,
+		position::Vec = O,
+		velocity::Vec = O)
+	p = Particle(mass = mass, charge = charge, radius = radius)
+	S.state[p] = (position, velocity)
+end
+
+function add_force(S::System, F::Force)
+	for p in S.particles
+		if can_act_on(F, p)
+			push!(p.forces, F)
+		end
+	end
+end
+
+@kwdef mutable struct Particle
 	mass = 1
 	charge = 0
 	radius = 0
+	forces::Set = Set()
 end
 
-struct Force
-	parameters::Dict
-	evaluate::Function
-	can_act_on::Function
+function position(p::Particle, state::Dict)
+	position, velocity = state[p]
+	return position
 end
+
+function velocity(p::Particle, state::Dict)
+	position, velocity = state[p]
+	return velocity
+end
+
+#### Uniform Gravitational Field ####
+
+@kwdef struct UniformGravity <: Force
+	g = 9.8
+	up = Z
+end
+
+function apply(F::UniformGravity, p::Particle, state::Dict)
+	return - p.mass * F.g * F.up
+end
+
+can_act_on(F::UniformGravity, p::Particle) = true
