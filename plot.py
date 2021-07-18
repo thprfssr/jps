c0cc518b2adef8c46e7983883ff3673d0a63d248
Javier Castro <jc@javiercastro.xyz>
2021-07-18 12:03:36 -0400

    modified:   plot.py

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import pandas as pd
 
-filename = 'smol.tsv'
+filename = 'output.tsv'
 df = pd.read_csv(filename, sep = '\t')
 times = list(set(df['t']))
 pids = list(set(df['id']))
@@ -21,8 +21,12 @@ def get_particle_state(pid, t):
     return rx, ry, rz, vx, vy, vz
 
 fig = plt.figure()
-ax = plt.axes(projection = '3d')
+#ax = plt.axes(projection = '3d')
+ax = plt.axes()
 
+line, = ax.plot([], [], 'o')
+
+'''
 X, Y, Z = [], [], []
 t = times[-1]
 for p in pids:
@@ -32,6 +36,21 @@ for p in pids:
     Z.append(rz)
 
 #ax.plot_trisurf(X, Y, Z)
-ax.scatter(X, Y, Z)
-plt.show()
+#ax.scatter(X, Y, Z)
+#plt.show()
+'''
+
+def animate(i):
+    t = times[i]
+    X = [get_particle_state(p, t)[0] for p in pids]
+    Y = [get_particle_state(p, t)[1] for p in pids]
+    Z = [get_particle_state(p, t)[2] for p in pids]
+
+    line.set_data(X, Y)
+    return line
+
+ani = animation.FuncAnimation(
+        fig, animate, 300)
+#plt.show()
+ani.save('test.mp4')
 
