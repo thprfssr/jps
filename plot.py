b58eeda137d4870dbb5eb9620fbcaff00d0908d0
Javier Castro <jc@javiercastro.xyz>
2021-08-26 14:35:39 -0400

    Played around to try to improve `plot.py`.

--- plot.py
+++ plot.py
@@ -10,8 +10,14 @@ pids = list(set(df['id']))
 times.sort()
 pids.sort()
 
-def get_particle_state(pid, t):
-    filtered = df.loc[(df['t'] == t) & (df['id'] == pid)]
+
+N = len(df) // 110
+STATES = [None] * N
+for i in range(N):
+    STATES[i] = df.iloc[i*110 : i*110 + 109]
+
+def get_particle_state(pid, i):
+    filtered = STATES[i].loc[df['id'] == pid]
     rx = sum(filtered['rx']) # We sum because there is only one row in the
     ry = sum(filtered['ry']) # filtered data, and we need to convert that
     rz = sum(filtered['rz']) # single row to a scalar, and also because
@@ -42,13 +48,16 @@ for p in pids:
 '''
 
 def animate(i):
-    t = times[i]
-    X = np.array([get_particle_state(p, t)[0] for p in pids])
-    Y = np.array([get_particle_state(p, t)[1] for p in pids])
-    Z = np.array([get_particle_state(p, t)[2] for p in pids])
+    X = np.array([get_particle_state(p, i)[0] for p in pids])
+    Y = np.array([get_particle_state(p, i)[1] for p in pids])
+    Z = np.array([get_particle_state(p, i)[2] for p in pids])
 
     sct.set_data(X, Y)
     sct.set_3d_properties(Z)
+
+
+    print(i)
+
     return sct
 
 
@@ -57,7 +66,7 @@ ax.set_ylim(-6,6)
 ax.set_zlim(-12,12)
 
 ani = animation.FuncAnimation(
-        fig, animate, 300)
+        fig, animate, 1000)
 #plt.show()
 ani.save('test.mp4')
 
